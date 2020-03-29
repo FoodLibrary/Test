a0907325a9151b3c218e94d9a51fc94b29f38fd0
천수빈 <suebin710@gmail.com>
2020-03-29 20:16:03 +0900

    Initial Settings - Spring Boot - React - Maven

---
+++ food-library/src/main/java/com/foodlibrary/foodlibrary/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.foodlibrary.foodlibrary;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FoodLibraryApplication.class);
+	}
+
+}
---
+++ food-library/src/test/java/com/foodlibrary/foodlibrary/FoodLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodlibrary.foodlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
