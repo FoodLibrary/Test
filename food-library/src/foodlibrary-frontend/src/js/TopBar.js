c49cbd1ae3f1f585dd9359af58e0861953b9f496
천수빈 <suebin710@gmail.com>
2020-04-04 23:54:16 +0900

    상단바 구성 및 스타일 생성

---
+++ food-library/src/foodlibrary-frontend/src/js/TopBar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import RankingBar from "./RankingBar";
+import { Button } from 'reactstrap';
+const imageResources = require('../util/ImageResources.js');
+
+class TopBar extends React.Component {
+    render() {
+        return (
+            <div className={"topBar"}>
+                <div>
+                    <img src={imageResources.categoryImg} id = {"categoryImg"} />
+                </div>
+                <div>
+                    <img src={imageResources.logoImg} id={"logoImg"}/>
+                </div>
+                <div id={"title"}>
+                    음식도서관
+                </div>
+                <div id={"searchArea"}>
+                    <input type={"text"} placeholder={"검색어를 입력하세요."} id={"searchTextInput"}/>
+                </div>
+                <div id={"searchButtonArea"}>
+                    <input type={"image"} src={imageResources.searchButtonImg} id={"searchButton"}/>
+                </div>
+                <div id={"rankingToggle"}>
+                    <RankingBar/>
+                </div>
+                <div id={"loginButtonArea"}>
+                    <Button className={"loginButton"}>로그인</Button>{' '}
+                </div>
+                <div id={"myPageButtonArea"}>
+                    <input type={"image"} src={imageResources.notMemberImage} id={"myPageButton"}/>
+                </div>
+
+            </div>
+
+        );
+    }
+
+}
+export default TopBar;
