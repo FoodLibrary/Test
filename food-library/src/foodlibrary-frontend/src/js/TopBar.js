fe39fc6593fab4767be9e7228aa576dc872b6a58
천수빈 <suebin710@gmail.com>
2020-04-11 20:35:44 +0900

    Top Bar Component + Styles

--- food-library/src/foodlibrary-frontend/src/js/TopBar.js
+++ food-library/src/foodlibrary-frontend/src/js/TopBar.js
@@ -1,22 +1,16 @@
 import React, { Component, useState } from 'react';
 import Navbar from 'react-bootstrap/Navbar'
-import Dropdown from "reactstrap/es/Dropdown";
-import { Form, FormControl} from "react-bootstrap";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import {Form, FormControl, Image} from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
 import Button from "react-bootstrap/Button";
-import NavItem from "react-bootstrap/NavItem";
-import NavLink from "react-bootstrap/NavLink";
-import Collapse from "react-bootstrap/Collapse";
-import NavbarToggler from "reactstrap/es/NavbarToggler";
-import ButtonDropdown from "reactstrap/es/ButtonDropdown";
+import RankingBar from "./RankingBar";
 const imageResources = require('../util/ImageResources.js');
 
 export default class TopBar extends Component {
     render() {
 
         return (
-            <Navbar expand="md">
+            <Navbar expand="md" className={"topBar"}>
                 <button className={"categoryImgArea"}> <img src={imageResources.categoryImg} className={"categoryImgArea"}/> </button>
                 <Navbar.Brand href="#home" className={"title"}> <img src={imageResources.logoImg} id={"logoImg"}/> <span id={"title"}> 음식도서관 </span> </Navbar.Brand>
 
@@ -26,11 +20,9 @@ export default class TopBar extends Component {
                         <Button className={"searchButton"}><img src={imageResources.searchButtonImg} id={"searchButton"}/></Button>
                     </Form>
                 </Navbar.Collapse>
-                <Nav>
-                    <Nav.Link> Login </Nav.Link>
-                    <Nav.Link> MyPage </Nav.Link>
-                </Nav>
-
+                <Nav> <RankingBar/> </Nav>
+                    <Nav.Link id={"loginButton"}> <span id={"login"}> 로그인 </span> </Nav.Link>
+                    <Nav.Link> <span id={"memberImageArea"}> <img src={imageResources.notMemberImage} className={"memberImage"}/> </span> </Nav.Link>
             </Navbar>
         );
     }
