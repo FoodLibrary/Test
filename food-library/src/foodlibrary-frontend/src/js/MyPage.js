0f96940a1b9c698dca31ccac9aa5a918d596afc0
천수빈 <suebin710@gmail.com>
2020-04-11 20:33:09 +0900

    My Page Component + Styles

--- food-library/src/foodlibrary-frontend/src/js/MyPage.js
+++ food-library/src/foodlibrary-frontend/src/js/MyPage.js
@@ -1,48 +1,37 @@
 import React from 'react';
 import { Button } from 'reactstrap';
-import TopBar from "./TopBar";
 import Filtering from "./Filtering";
-import ProductList from "./ProductList";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 const imageResources = require('../util/ImageResources.js');
 
 class MyPage extends React.Component {
     render() {
         return (
-            <div>
-                <div>
-                    <TopBar/>
-                </div>
-                <div id={"myPageTitle"}>
-                    마이페이지
-                </div>
-                <div>
-                    <span id={"ageAndSexInfoText"}> 20대/여자 </span>
-                    <span id={"memberImageArea"}> <img src={imageResources.memberImage} id={"memberImage"}/> </span>
-                    <span id={"editMemberInfo"}> <Button className={"editMemberInfoButton"}> 회원 정보 수정 </Button> </span>
-                </div>
-                <div>
-                    <hr id={"lineLeft"}/>
-                    <hr id={"lineRight"}/>
-                </div>
-                <div>
+            <Container>
+                <Row>
+                    <Col xs={{size:4, offset:4}} md={{size:4,offset:4}} lg={{size:4,offset:4}}> <span id={"myPageTitle"}> 마이페이지 </span> </Col>
+                </Row>
+                <Row> <Col xs={{size:5, offset:4}} className={"memberImageEdit"}> <span> <img src={imageResources.memberImage} id={"memberImage"}/> </span> </Col> </Row>
+                <Row>
+                    <Col xs={{size:3, offset:1}} md={{size:3, offset:3}} className={"ageAndSexInfoText"}> <span id={"ageAndSexInfoText"}> 20대/여자 </span> </Col>
+                    <Col xs={{size:6}} md={{size:3, offset:3}} className={"editMemberInfoButton"}> <span> <Button id={"editMemberInfoButton"}> 회원  정보 수정 </Button> </span> </Col>
+                </Row>
+                <Row>
                     <Button className={"myFilter"}>
                         <img src={imageResources.myFilter} id={"myFilterLogo"}/>
                         내 필터링
                     </Button>
-                </div>
-                <div>
-                    <Filtering/>
-                </div>
-                <div id={"myHeartArea"}>
+                </Row>
+                <Filtering/>
+                <Row>
                     <Button className={"myHeart"}>
                         <img src={imageResources.myHeart} id={"myHeartLogo"}/>
                         내 찜목록
                     </Button>
-                </div>
-                <div>
-                    <ProductList/>
-                </div>
-            </div>
+                </Row>
+            </Container>
 
 
         );
