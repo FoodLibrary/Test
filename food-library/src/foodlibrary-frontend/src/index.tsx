c1939072a01f296be49bb704e506ee75ff25e61f
Kyeongukna <ruddnr96@naver.com>
2020-04-16 01:03:44 +0900

    index commit

--- food-library/src/foodlibrary-frontend/src/index.tsx
+++ food-library/src/foodlibrary-frontend/src/index.tsx
@@ -4,35 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'reactstrap';
 import * as serviceWorker from './serviceWorker';
 
+
 /*
-
-import './SearchResult/ResultIndex.css';
-import ResultFiltering from './SearchResult/ResultFiltering';
-import SearchResult from './SearchResult/SearchResult';
-
-
-
-ReactDOM.render(
-    <React.StrictMode>
-        <ResultFiltering />
-        <SearchResult />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
-*/
-/*
-
-import Main from './Main/Main'
-
-ReactDOM.render(
-    <React.StrictMode>
-        <Main />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
-*/
-
-
+//랭킹 화면
 import './Ranking/Ranking.css';
 import Ranking from './Ranking/Ranking';
 
@@ -44,6 +18,61 @@ ReactDOM.render(
 );
 
 
+*/
+/*
+//메인화면
+import Main from './Main/Main'
+
+ReactDOM.render(
+    <React.StrictMode>
+        <Main />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+*/
+
+/*
+
+*/
+
+/*
+//로그인 화면
+import './index.css';
+import LoginPage from './sj/LoginPage';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <LoginPage />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+*/
+
+/*
+//상품상세화면
+import './index.css';
+import ProductPage from './sj/ProductPage';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <ProductPage />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+*/
+//검색 결과 화면
+import './SearchResult/ResultIndex.css';
+import ResultFiltering from './SearchResult/ResultFiltering';
+import SearchResult from './SearchResult/SearchResult';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <ResultFiltering />
+        <SearchResult />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
