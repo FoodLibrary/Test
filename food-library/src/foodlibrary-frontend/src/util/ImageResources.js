4e5ab121dfe5ab76a9c26cbb290c50600b38346f
Kyeongukna <ruddnr96@naver.com>
2020-04-16 17:57:09 +0900

    ImageResource 추가 ( origin,emptyStar,filledStar,reviewImage,camera,hashTag,productImage)

--- food-library/src/foodlibrary-frontend/src/util/ImageResources.js
+++ food-library/src/foodlibrary-frontend/src/util/ImageResources.js
@@ -7,6 +7,15 @@ const memberImage = "https://cdn.zeplin.io/5e62877178f87615c993cd42/assets/1C3C4
 const myFilter = "https://cdn.zeplin.io/5e62877178f87615c993cd42/assets/117F0361-1F9E-40CD-82A1-C56C3E114CEF.png";
 const myHeart = "https://cdn.zeplin.io/5e62877178f87615c993cd42/assets/80406245-72B8-455B-BA53-B836563235E2.png";
 const thumb = "https://cdn.zeplin.io/5e62877178f87615c993cd42/assets/2F3ECBE2-8BFE-4633-8D6D-04C11E07A486.png";
+const heart = "https://cdn.zeplin.io/5e62877178f87615c993cd42/assets/80406245-72B8-455B-BA53-B836563235E2.png";
+const origin = "https://cdn.zeplin.io/5e62877178f87615c993cd42/assets/47CB0ABA-BDEE-452A-A6C9-B57CCC9DDFBA.png";
+const emptyStar ="https://cdn.zeplin.io/5e62877178f87615c993cd42/assets/454BE7F5-B6DA-4EAF-9156-D780CF571CA9.png";
+const filledStar = "https://cdn.zeplin.io/5e62877178f87615c993cd42/assets/3F452664-05BF-4410-9C9E-45C20756A9C9.png";
+const reviewImage = "https://cdn.zeplin.io/5e62877178f87615c993cd42/assets/4881970C-494C-43E8-90B3-B7E2E41EF263.png";
+const camera = "https://cdn.zeplin.io/5e62877178f87615c993cd42/assets/4B0F78FB-AE37-4803-8C75-2FD1D437B3B5.png";
+const hashTag = "https://cdn.zeplin.io/5e62877178f87615c993cd42/assets/6D32DDE3-F645-4BA4-A348-A0C208F74E61.png";
+const productImage = "https://cdn.zeplin.io/5e62877178f87615c993cd42/assets/46919DA9-54F9-42D2-AF84-07A47F7FE37C.png";
+
 
 module.exports = {
     categoryImg,
@@ -17,4 +26,12 @@ module.exports = {
     myFilter,
     myHeart,
     thumb,
+    heart,
+    origin,
+    emptyStar,
+    filledStar,
+    reviewImage,
+    camera,
+    hashTag,
+    productImage,
 };
