6804d30bc714de57ef0744554b9fe43967510da2
천수빈 <suebin710@gmail.com>
2020-04-27 15:45:20 +0900

    preparing word cloud

--- food-library/src/main/java/com/foodlibrary/foodlibrary/ApiController.java
+++ food-library/src/main/java/com/foodlibrary/foodlibrary/ApiController.java
@@ -1,14 +1,22 @@
 package com.foodlibrary.foodlibrary;
 
+import crawling.BlogCrawling;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.List;
+
 @RestController
 public class ApiController {
+    //WebDriver
+    private ChromeDriver driver;
+    BlogCrawling blogCrawling = new BlogCrawling();
 
     @GetMapping("/api/foodlibrary")
-    public String foodlibrary() {
-        return "아 좀 되라 씨빠";
+    public HashMap<String, Integer> foodlibrary() {
+        return blogCrawling.frequencyCount(blogCrawling.trimTag());
     }
 
 }
--- food-library/src/main/java/crawling/BlogCrawling.java
+++ food-library/src/main/java/crawling/BlogCrawling.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.StringTokenizer;
 
 @RestController
 public class BlogCrawling {
@@ -24,6 +26,12 @@ public class BlogCrawling {
     //검색할 키워드
     public String keyword = "홈런볼";
     public List<String> keywords = new ArrayList<>();
+
+    //태그 크롤링 결과 저장 리스트
+    public List<String> tagsResult = new ArrayList<>();
+
+    //중복 태그 워드 리스트
+    public List<String> trimTagResult = new ArrayList<>();
 
     public BlogCrawling() {
         super();
@@ -51,14 +59,41 @@ public class BlogCrawling {
         String mainFrame = driver.findElement(By.cssSelector("#mainFrame")).getAttribute("src");
         driver.get(mainFrame);
         List<WebElement> tags = driver.findElements(By.cssSelector(".ell"));
+
         for(WebElement tag : tags) {
             System.out.println(tag.getText());
+            tagsResult.add(tag.getText());
         }
         this.driver.close();
         this.driver.switchTo().window(windowHandles.get(0));
+
     }
 
-    @GetMapping("/api/hello")
+    public List<String> trimTag() {
+        for (int i = 0; i < tagsResult.size()-1; i++) {
+            for (int j = i+1; j < tagsResult.size() ; j++) {
+                if (tagsResult.get(i).equals(tagsResult.get(j)) && !(tagsResult.get(i).equals(""))) {
+                    trimTagResult.add(tagsResult.get(i));
+                }
+            }
+        }
+        return trimTagResult;
+    }
+
+    public HashMap<String, Integer> frequencyCount (List<String> list) {
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+        for (int i = 0; i < list.size(); i++) {
+            int x = 1;
+            if (map.containsKey(list.get(i))) {
+                x = map.get(list.get(i)) + 1;
+            }
+
+            map.put(list.get(i), x);
+        }
+        return map;
+    }
+
     public void crawl() {
         try {
             getPost(driver);
@@ -70,3 +105,4 @@ public class BlogCrawling {
     }
 
 }
+
